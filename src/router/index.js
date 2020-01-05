da827f0e1634f1ae9b8fa0544ed5616f95b31b1d
yuminghui <2404446044@qq.com>
2020-01-05 20:59:14 +0800

    项目初始化-调整目录结构

--- src/router/index.js
+++ src/router/index.js
@@ -1,18 +1,18 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import index from '../views/index.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: Home
+    name: 'index',
+    component: index
   },
   {
-    path: '/about',
-    name: 'about',
+    path: '/index',
+    name: 'index',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
